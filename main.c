0988f31de58863451448661866b5a9b55fc3a96e
Patryk Grzmil <pattzuris@gmail.com>
2020-10-08 21:31:24 +0200

    Kwadrat sam

--- main.c
+++ main.c
@@ -7,8 +7,12 @@ int main() {
     printf("Podaj dlugosc boku");
     scanf(" %d", &firstNumber);
 
-    for (int i = 0; i < firstNumber; ++i) {
+    for (int i = 0; i < firstNumber; i++) {
         printf("*");
+        for (int j = 0; j < firstNumber; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
     return 0;
 }
